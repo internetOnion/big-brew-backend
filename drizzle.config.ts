21d17d9b3ee2b3ec31022ebbb3c2824514ecd869
developOnion <smeyonion.nou@gmail.com>
2026-07-01 11:01:05 +0700

    Refactor JSON formatting in _journal.json for improved readability

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -2,8 +2,8 @@ import "dotenv/config";
 import { defineConfig } from "drizzle-kit";
 
 export default defineConfig({
-    schema: "./src/models/schema/index.ts",
-    out: "./src/models/migrations",
+    schema: "./src/shared/models/schema/index.ts",
+    out: "./src/shared/models/migrations",
     dialect: "postgresql",
     dbCredentials: {
         url: process.env.SUPABASE_DATABASE_URL!,
